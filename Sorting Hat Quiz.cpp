be3f46b195cdf3f6b93fd0445097e19c663fc71b
cdelangelm <cdelangelm@users.noreply.github.com>
2020-07-23 11:50:02 -0500

    Variables declared.

--- Sorting Hat Quiz.cpp
+++ Sorting Hat Quiz.cpp
@@ -4,4 +4,11 @@
 using namespace std;
 
 int main() {
+	// Variable declaration
+	int gryffindor = 0;
+	int hufflepuff = 0;
+	int ravenclaw = 0;
+	int slytherin = 0;
+
+	int answer1, answer2, answer3, answer4;
 }
