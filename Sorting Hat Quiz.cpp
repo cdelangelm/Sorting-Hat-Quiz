f8ee4b20976f6c2d05c385acfd494c02df55fe51
cdelangelm <cdelangelm@users.noreply.github.com>
2020-07-23 11:51:09 -0500

    Fist question added.

--- Sorting Hat Quiz.cpp
+++ Sorting Hat Quiz.cpp
@@ -11,4 +11,8 @@ int main() {
 	int slytherin = 0;
 
 	int answer1, answer2, answer3, answer4;
+
+	// First question
+	cout << "The Sorting Hat quiz!\n\n";
+	cout << "Q1) When I'm dead, I want people to remember me as:\n\n" << "  1) The Good\n" << "  2) The Great\n" << "  3) The Wise\n" << "  4) The Bold\n";
 }
