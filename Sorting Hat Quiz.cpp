42d0661109dd759350e238013897a6bd4b1dcea1
cdelangelm <cdelangelm@users.noreply.github.com>
2020-07-24 10:53:09 -0500

    Added for loop and questionLogic function

--- Sorting Hat Quiz.cpp
+++ Sorting Hat Quiz.cpp
@@ -1,9 +1,9 @@
-// Sorting Hat Quiz project Codecademy
-
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main() {
+int questionLogic(int answer, int house1, int house2, int house3, int house4);
+
+int main(){
 	// Variable declaration
 	int gryffindor = 0;
 	int hufflepuff = 0;
@@ -12,7 +12,38 @@ int main() {
 
 	int answer1, answer2, answer3, answer4;
 
-	// First question
+	// Program title
 	cout << "The Sorting Hat quiz!\n\n";
-	cout << "Q1) When I'm dead, I want people to remember me as:\n\n" << "  1) The Good\n" << "  2) The Great\n" << "  3) The Wise\n" << "  4) The Bold\n";
+
+	// First question
+	for (;;) {
+		cout << "Q1) When I'm dead, I want people to remember me as:\n\n" << "  1) The Good\n" << "  2) The Great\n" << "  3) The Wise\n" << "  4) The Bold\n";
+		cin >> answer1;
+		if (answer1 > 4) {
+			break;
+		}
+		else {
+			cout << "Invalid output, try again";
+			continue;
+		}
+	}
+	//First question logic
+	questionLogic(answer1, hufflepuff, slytherin, ravenclaw, gryffindor);
+	cout << hufflepuff << " " << slytherin << " " << ravenclaw << " " << gryffindor;
+}
+
+int questionLogic(int answer, int house1, int house2, int house3, int house4) {
+	if (answer == 1){
+		house1++;
+	}
+	else if (answer == 2) {
+		house2++;
+	}
+	else if (answer == 3) {
+		house3++;
+	}
+	else if (answer == 4) {
+		house4++;
+	}
+	return;
 }
